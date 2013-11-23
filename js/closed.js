d87f9ed366a03e70a08e8df15911d3d467367d94
Evan Duncan <evan.e.duncan@gmail.com>
2013-11-23 13:56:04 -0600

    'Added closed page'

---
+++ js/closed.js
@@ -0,0 +1,32 @@
+(function($) { 
+  $(document).ready(function() {
+    var source = $('#lot-template').html();
+    var template = Handlebars.compile(source);
+
+    $('.lot-container').on('click', '.details-link', function(e) {
+      var id = $(this).find('.lot-id').val();
+      document.location = '/lot/details/' + id;
+    });
+
+		$.ajax({
+			type: "GET",
+			url: "/api/lots/closed",
+			dataType: "json"
+		}).done(function(lots) {
+      console.log('done');
+      $.each(lots, function(i, l){
+		console.log(l);
+        $('.lot-container').append(template({lot: l}));
+      });
+		}).fail(function(x, e, d) {
+      console.log('fail');
+      console.log(x);
+      console.log(e);
+      console.log(d);
+    }).always(function() {
+      console.log('always');
+    });
+
+	});
+})(jQuery)
+
