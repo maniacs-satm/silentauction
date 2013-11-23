aa37cc39706b643454dd7dab9584bbd8e188c733
satrent <trent@copperconsultingmn.com>
2013-11-22 22:12:35 -0600

    set up the menu

--- js/layout.js
+++ js/layout.js
@@ -12,16 +12,32 @@
         $('#headerLogin').hide();
         $('#headerLogout').show();
         $('.header-right').show();
+        $('.logged-in').show();
+        $('.logged-out').hide();
       }
       else {
         $('#headerLogin').show();
         $('#headerLogout').hide();
         $('.header-right').hide();
+        $('.logged-out').hide();
+        $('.logged-in').show();
       }
     });
 
     $('#headerLogin').click(function(e) {
       document.location = '/login';
+    });
+
+    $('#headerMenu').click(function(e) {
+      if($('.header-menu').is(':hidden')) {
+        $('.header-menu').slideDown(function() {
+          $('#menuImage').attr('src', '/css/images/up-arrow.png');
+        });
+      } else {
+        $('.header-menu').slideUp(function() {
+          $('#menuImage').attr('src', '/css/images/down-arrow.png');
+        });
+      }
     });
 
     $('#headerLogout').click(function(e) {
